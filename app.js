0a12f88627580a8b59ee3d603c05ec87c33f0f52
umashankar <umashankarp33@gmail.com>
2025-01-15 09:29:02 +0530

    add signup and login controllers and add global error handler

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import db from './config/db.js';
-import userRouter from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 db();
 
 let app=express();
@@ -8,6 +8,11 @@ let app=express();
 app.use(express.json())
 
 //base route
-app.use("/api/v1/user",userRouter)
+app.use("/api/v1/user",userRoutes)
 
+//global error handling middleware
+app.use((err,req,res,next)=>{
+    console.log(err.stack);
+    return res.status(400).json(err.message);
+})
 export default app;
